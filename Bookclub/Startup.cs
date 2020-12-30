0833e917b8e6238bd566d9aeb50ab6b692e16180
nickguerra <nickguerra@gmail.com>
2020-12-30 09:48:36 -0600

    test commit with new code

--- Bookclub/Startup.cs
+++ Bookclub/Startup.cs
@@ -22,8 +22,6 @@ namespace Bookclub
 
       public IConfiguration Configuration { get; }
 
-      // This method gets called by the runtime. Use this method to add services to the container.
-      // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
       public void ConfigureServices(IServiceCollection services)
       {
          services.AddRazorPages();
