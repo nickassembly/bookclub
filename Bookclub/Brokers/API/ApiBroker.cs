e1b7be3cea09849014c5d5eb4c82c29169350e82
nickguerra <nickguerra@gmail.com>
2021-01-02 07:22:43 -0600

    added test project and restfulsense library

--- Bookclub/Brokers/API/ApiBroker.cs
+++ Bookclub/Brokers/API/ApiBroker.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RESTFulSense.Clients;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,21 @@ namespace Bookclub.Brokers.API
 {
     public class ApiBroker : IApiBroker
     {
-        
-    }
+      private readonly IRESTFulApiFactoryClient apiClient;
+
+      public ApiBroker(IRESTFulApiFactoryClient apiClient) =>
+         this.apiClient = apiClient;
+
+      private async ValueTask<T> GetAsync<T>(string relativeUrl) =>
+         await this.apiClient.GetContentAsync<T>(relativeUrl);
+
+      private async ValueTask<T> PostAsync<T>(string relativeUrl, T content) =>
+         await this.apiClient.PostContentAsync<T>(relativeUrl, content);
+
+      private async ValueTask<T> PutAsync<T>(string relativeUrl, T content) =>
+         await this.apiClient.PutContentAsync<T>(relativeUrl, content);
+
+      private async ValueTask<T> DeleteAsync<T>(string relativeUrl) =>
+         await this.apiClient.DeleteContentAsync<T>(relativeUrl);
+   }
 }
--- Bookclub/Startup.cs
+++ Bookclub/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RESTFulSense.Clients;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,14 +29,19 @@ namespace Bookclub
 
          // Adding HTTP client to communicate with Api (possibly add in RESTFULsense library once I understand it)
          // for now just using a generic client
+
          // TODO: need to find out best httpclient to use
          //services.AddHttpClient();
-         services.AddHttpClient<IHttpClientBuilder, IHttpClientBuilder>(client => 
-         {
-            LocalConfigurations localConfigurations = Configuration.Get<LocalConfigurations>();
-            string apiUrl = localConfigurations.ApiConfigurations.Url;
-            client.BaseAddress = new Uri(apiUrl);
-         });
+         //services.AddHttpClient<IHttpClientBuilder, IHttpClientBuilder>(client => 
+         //{
+         //   LocalConfigurations localConfigurations = Configuration.Get<LocalConfigurations>();
+         //   string apiUrl = localConfigurations.ApiConfigurations.Url;
+         //   client.BaseAddress = new Uri(apiUrl);
+         //});
+
+         // Restfulsense client (library)
+         AddHttpClient(services);
+         AddRootDirectory(services);
 
          services.AddRazorPages(options =>
          {
@@ -66,6 +72,25 @@ namespace Bookclub
          {
             endpoints.MapBlazorHub();
             endpoints.MapFallbackToPage("/_Host");
+         });
+      }
+
+      private void AddHttpClient(IServiceCollection services)
+      {
+         services.AddHttpClient<IRESTFulApiFactoryClient, RESTFulApiFactoryClient>(client =>
+         {
+            LocalConfigurations localConfigurations = Configuration.Get<LocalConfigurations>();
+            string apiUrl = localConfigurations.ApiConfigurations.Url;
+            client.BaseAddress = new Uri(apiUrl);
+
+         });
+      }
+
+      private static void AddRootDirectory(IServiceCollection services)
+      {
+         services.AddRazorPages(OptionsBuilderConfigurationExtensions =>
+         {
+            OptionsBuilderConfigurationExtensions.RootDirectory = "/Views/Pages";
          });
       }
    }
