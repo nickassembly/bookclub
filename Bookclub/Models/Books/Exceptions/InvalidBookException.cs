1e2fc1678b3e1ccbfb17c3c2c10fda22d6028538
nickguerra <nickguerra@gmail.com>
2021-01-10 07:46:31 -0600

    added invalid book exception object

---
+++ Bookclub/Models/Books/Exceptions/InvalidBookException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookclub.Models.Books.Exceptions
+{
+    public class InvalidBookException : Exception
+    {
+        public InvalidBookException(string parameterName, string parameterValue)
+            :base("Invalid Book error occurred," +
+                 $"parameter name: {parameterName}" +
+                 $"parameter value: {parameterValue}")
+        { }
+    }
+}
