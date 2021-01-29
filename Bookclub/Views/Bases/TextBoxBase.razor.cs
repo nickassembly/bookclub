38d3611dd0f43edf2d3877484288c75207073639
nickguerra <nickguerra@gmail.com>
2021-01-29 09:22:13 -0600

    added first base component

--- Bookclub/Views/Bases/TextBoxBase.razor.cs
+++ Bookclub/Views/Bases/TextBoxBase.razor.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Components;
 
 namespace Bookclub.Views.Bases
 {
     public partial class TextBoxBase
     {
-        
+        [Parameter]
+        public string Value { get; set; }
+
+        public void SetValue(string value) =>
+            this.Value = value;
     }
 
 }
