2106e1ef56fc88bf2ca2bf1ef9bd914a5923ace2
nickguerra <nickguerra@gmail.com>
2021-02-13 07:50:22 -0600

    Added class stub test render exceptions

---
+++ Bookclub.Tests/Views/AddBookComponents/AddBookComponentTests.Exceptions.cs
@@ -0,0 +1,32 @@
+﻿using Bookclub.Models.Books.BookViews;
+using Bookclub.Models.ContainerComponents;
+using Bookclub.Views.Components;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Bookclub.Tests.Views.AddBookComponents
+{
+    public partial class AddBookComponentTests
+    {
+
+        [Fact]
+        public void ShouldSubmitBook()
+        {
+        
+            // given
+
+            // when
+
+            // then
+
+
+        }
+
+    }
+}
