6cf22b3cf4125edc704bb48592c6cb98fe26ffe0
Joseph Guerra <guerra.joseph@gmail.com>
2021-03-19 11:16:14 -0500

    Updated Bookview model to default publishdatetime component to today's date

--- Bookclub/Models/Books/BookViews/BookView.cs
+++ Bookclub/Models/Books/BookViews/BookView.cs
@@ -14,7 +14,8 @@ namespace Bookclub.Models.Books.BookViews
         public string Title { get; set; }
         public string Subtitle { get; set; }
         public string PrimaryAuthor { get; set; }
-        public DateTimeOffset PublishedDate { get; set; }
+        public DateTimeOffset PublishedDate { get; set; } = DateTime.Now;
+
         public BookViewMediaType MediaType { get; set; }
 
     }
