c83ea1d4a1f9fa22e22756e86b508607fecf9be9
nickguerra <nickguerra@gmail.com>
2021-03-21 13:12:44 -0500

    added user model class stubs

---
+++ Bookclub/Models/Users/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookclub.Models.Users
+{
+    public class User
+    {
+        // TODO: Need to create user model
+    }
+}
