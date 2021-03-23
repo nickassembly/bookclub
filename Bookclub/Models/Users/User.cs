95721188b2fe6edf8f41e8598b8d059a2c5e7bd0
nickguerra <nickguerra@gmail.com>
2021-03-22 19:06:08 -0500

    added user model

--- Bookclub/Models/Users/User.cs
+++ Bookclub/Models/Users/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,10 @@ using System.Threading.Tasks;
 
 namespace Bookclub.Models.Users
 {
-    public class User
+    public class User 
     {
-        // TODO: Need to create user model
+        public string Login { get; set; }
+        public string Password { get; set; }
+
     }
 }
