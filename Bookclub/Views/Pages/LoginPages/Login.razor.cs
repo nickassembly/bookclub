cc350ac7a0fde606def21402b4ec915a6a0d1abe
nickguerra <nickguerra@gmail.com>
2021-03-28 07:39:59 -0500

    moved login code to its own file

---
+++ Bookclub/Views/Pages/LoginPages/Login.razor.cs
@@ -0,0 +1,67 @@
+﻿using Bookclub.Data;
+using Bookclub.Models.Users;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookclub.Views.Pages.LoginPages
+{
+    public partial class Login
+    {
+        private User user;
+        public string LoginMessage { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+
+            user = new User();
+            return base.OnInitializedAsync();
+
+        }
+
+        private async Task<bool> ValidateUser()
+        {
+            // Serialize user object
+            string serializedUser = JsonConvert.SerializeObject(user);
+
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage();
+            httpRequestMessage.Method = new HttpMethod("POST");
+            httpRequestMessage.RequestUri = new Uri("https://bookclubapiservicev2.azurewebsites.net/api/users");
+            httpRequestMessage.Content = new StringContent(serializedUser);
+
+            httpRequestMessage.Content.Headers.ContentType =
+                new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            var response = await Http.SendAsync(httpRequestMessage);
+
+            var responseStatusCode = response.StatusCode;
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            if (response.StatusCode.ToString() == "OK")
+            {
+                var returnedUser = JsonConvert.DeserializeObject<User>(responseBody);
+
+                await sessionStorage.SetItemAsync("emailAddress", user.EmailAddress);
+                await sessionStorage.SetItemAsync("token", user.AccessToken);
+
+                ((CustomAuthenticationStateProvider)AuthenticationStateProvider).MarkUserAsAuthenticated(user.EmailAddress);
+                NavigationManager.NavigateTo("/index");
+            }
+            else
+            {
+                LoginMessage = "Invalid username or password";
+            }
+
+            // TODO: Remove temporary code once API has proper login endpoint
+            ((CustomAuthenticationStateProvider)AuthenticationStateProvider).MarkUserAsAuthenticated(user.EmailAddress);
+            NavigationManager.NavigateTo("/index");
+            await sessionStorage.SetItemAsync("emailAddress", user.EmailAddress);
+
+            return await Task.FromResult(true);
+        }
+    }
+}
