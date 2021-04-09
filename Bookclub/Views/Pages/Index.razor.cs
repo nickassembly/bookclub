4b0a425dea5efcc3a8ae3c6e72365d1cc9796f05
nickguerra <nickguerra@gmail.com>
2021-04-09 06:51:01 -0500

    added code behind file to index.razor

---
+++ Bookclub/Views/Pages/Index.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookclub.Views.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        public List<int> Numbers { get; set; } = Enumerable.Range(start: 0, count: 10).ToList();
+    }
+}
