71bc0ecb63579ddb3e992c3761233fbd437c0c53
nickguerra <nickg@maliciousgames.com>
2021-05-05 06:18:47 -0500

    added api call to users endpoint to pull back all users

--- Bookclub/Services/BookViews/BookViewService.cs
+++ Bookclub/Services/BookViews/BookViewService.cs
@@ -3,9 +3,11 @@ using Bookclub.Brokers.DateTimes;
 using Bookclub.Brokers.Logging;
 using Bookclub.Models.Books;
 using Bookclub.Models.Books.BookViews;
+using Bookclub.Models.Users;
 using Bookclub.Services.Books;
 using Bookclub.Services.Users;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Session;
 using System;
 using System.Collections.Generic;
@@ -21,6 +23,7 @@ namespace Bookclub.Services.BookViews
         private readonly IDateTimeBroker _dateTimeBroker;
         private readonly ILoggingBroker _loggingBroker;
         private readonly ISessionStorageService _sessionStorage;
+        private readonly IHttpContextAccessor _ctx;
 
 
         private readonly HttpClient _httpClient;
@@ -31,7 +34,8 @@ namespace Bookclub.Services.BookViews
             IDateTimeBroker dateTimeBroker,
             ILoggingBroker loggingBroker,
             HttpClient httpClient,
-            ISessionStorageService sessionStorage)
+            ISessionStorageService sessionStorage,
+            IHttpContextAccessor ctx)
         {
             _bookService = bookService;
             _userService = userService;
@@ -39,6 +43,7 @@ namespace Bookclub.Services.BookViews
             _loggingBroker = loggingBroker;
             _httpClient = httpClient;
             _sessionStorage = sessionStorage;
+            _ctx = ctx;
         }
 
         public ValueTask<BookView> AddBookViewAsync(BookView bookView) =>
@@ -58,10 +63,10 @@ namespace Bookclub.Services.BookViews
 
         private Book MapToBook(BookView bookView)
         {
-
+            // TODO: Email is not being handled correctly. Need a better way to maintain session data
             string userEmail = _sessionStorage.GetItemAsync<string>("email").ToString();
 
-            // TODO: Add rest sharp to make call to get user through email
+            User loggedInUser = _userService.GetCurrentlyLoggedInUser(_ctx.HttpContext, userEmail);
 
             DateTimeOffset currentDateTime = _dateTimeBroker.GetCurrentDateTime();
 
--- Bookclub/Services/Users/IUserService.cs
+++ Bookclub/Services/Users/IUserService.cs
@@ -1,4 +1,5 @@
 ﻿using Bookclub.Models.Users;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,6 @@ namespace Bookclub.Services.Users
 {
     public interface IUserService
     {
-        User GetCurrentlyLoggedInUser(string email);
+        User GetCurrentlyLoggedInUser(HttpContext ctx, string email);
     }
 }
