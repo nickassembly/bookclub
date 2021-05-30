1f40d296f467eb89ed724bda507ac2a73da8c5b2
nickguerra <nickg@maliciousgames.com>
2021-05-30 05:45:13 -0500

    refactored edit component for cleaner code

--- Bookclub/Views/Components/EditBookComponent.razor.cs
+++ Bookclub/Views/Components/EditBookComponent.razor.cs
@@ -24,127 +24,23 @@ namespace Bookclub.Views.Components
 
         [Parameter]
         public string BookListPrice { get; set; }
+        public ComponentState State { get; set; }
+       // public AddBookComponentException Exception { get; set; }
+        public BookView BookView { get; set; }
+        public TextBoxBase IsbnTextBox { get; set; }
+        public TextBoxBase Isbn13TextBox { get; set; }
+        public TextBoxBase TitleTextBox { get; set; }
+        public TextBoxBase SubtitleTextBox { get; set; }
+        public TextBoxBase AuthorTextBox { get; set; }
+        //public DropDownBase<BookViewMediaType> MediaTypeDropDown { get; set; }
+        public TextBoxBase PublisherTextBox { get; set; }
+        public TextBoxBase ListPrice { get; set; }
+        public DatePickerBase PublishDatePicker { get; set; }
+        public ButtonBase ConfirmEditButton { get; set; }
+        public ButtonBase CancelEditButton { get; set; }
+        public LabelBase StatusLabel { get; set; }
 
-        private string _isbnInput;
-        [Parameter]
-        public string IsbnInput
-        {
-            get => _isbnInput;
-            set
-            {
-                if (_isbnInput == value) return;
-                _isbnInput = value;
-                IsbnInputChanged.InvokeAsync(value);
-            }
-        }
-        [Parameter]
-        public EventCallback<string> IsbnInputChanged { get; set; }
-
-        private string _isbn13Input;
-        [Parameter]
-        public string Isbn13Input
-        {
-            get => _isbn13Input;
-            set
-            {
-                if (_isbn13Input == value) return;
-                _isbn13Input = value;
-                Isbn13InputChanged.InvokeAsync(value);
-            }
-        }
-        [Parameter]
-        public EventCallback<string> Isbn13InputChanged { get; set; }
-
-        private string _authorInput;
-        [Parameter]
-        public string AuthorInput
-        {
-            get => _authorInput;
-            set
-            {
-                if (_authorInput == value) return;
-                _authorInput = value;
-                AuthorInputChanged.InvokeAsync(value);
-            }
-        }
-        [Parameter]
-        public EventCallback<string> AuthorInputChanged { get; set; }
-
-        private string _titleInput;
-        [Parameter]
-        public string TitleInput
-        {
-            get => _titleInput;
-            set
-            {
-                if (_titleInput == value) return;
-                _titleInput = value;
-                TitleInputChanged.InvokeAsync(value);
-            }
-        }
-        [Parameter]
-        public EventCallback<string> TitleInputChanged { get; set; }
-
-        private string _subTitleInput;
-        [Parameter]
-        public string SubTitleInput
-        {
-            get => _subTitleInput;
-            set
-            {
-                if (_subTitleInput == value) return;
-                _subTitleInput = value;
-                SubTitleInputChanged.InvokeAsync(value);
-            }
-        }
-        [Parameter]
-        public EventCallback<string> SubTitleInputChanged { get; set; }
-
-        private string _mediaTypeInput;
-        [Parameter]
-        public string MediaTypeInput
-        {
-            get => _mediaTypeInput;
-            set
-            {
-                if (_mediaTypeInput == value) return;
-                _mediaTypeInput = value;
-                MediaTypeInputChanged.InvokeAsync(value);
-            }
-        }
-        [Parameter]
-        public EventCallback<string> MediaTypeInputChanged { get; set; }
-
-        private string _publisherInput;
-        [Parameter]
-        public string PublisherInput
-        {
-            get => _publisherInput;
-            set
-            {
-                if (_publisherInput == value) return;
-                _publisherInput = value;
-                PublisherInputChanged.InvokeAsync(value);
-            }
-        }
-        [Parameter]
-        public EventCallback<string> PublisherInputChanged { get; set; }
-
-        private string _listPriceInput;
-        [Parameter]
-        public string ListPriceInput
-        {
-            get => _listPriceInput;
-            set
-            {
-                if (_listPriceInput == value) return;
-                _listPriceInput = value;
-                ListPriceInputChanged.InvokeAsync(value);
-            }
-        }
-        [Parameter]
-        public EventCallback<string> ListPriceInputChanged { get; set; }
-
+        // TODO: Need better way to handle publish date picker
         private DateTimeOffset _publishDateInput;
         [Parameter]
         public DateTimeOffset PublishDateInput
@@ -160,21 +56,12 @@ namespace Bookclub.Views.Components
         [Parameter]
         public EventCallback<DateTimeOffset> PublishDateInputChanged { get; set; }
 
-        public ComponentState State { get; set; }
-        public AddBookComponentException Exception { get; set; }
-        public BookView BookView { get; set; }
-        public TextBoxBase IsbnTextBox { get; set; }
-        public TextBoxBase Isbn13TextBox { get; set; }
-        public TextBoxBase TitleTextBox { get; set; }
-        public TextBoxBase SubtitleTextBox { get; set; }
-        public TextBoxBase AuthorTextBox { get; set; }
-        public DropDownBase<BookViewMediaType> MediaTypeDropDown { get; set; }
-        public TextBoxBase Publisher { get; set; }
-        public TextBoxBase ListPrice { get; set; }
-        public DatePickerBase PublishDatePicker { get; set; }
-        public ButtonBase ConfirmEditButton { get; set; }
-        public ButtonBase CancelEditButton { get; set; }
-        public LabelBase StatusLabel { get; set; }
+        private string Isbn { get; set; }
+        private string Isbn13 { get; set; }
+        private string Author { get; set; }
+        private string Title { get; set; }
+        private string Subtitle { get; set; }
+        private string Publisher { get; set; }
 
         protected override void OnInitialized()
         {
@@ -194,25 +81,25 @@ namespace Bookclub.Views.Components
                 // TODO: Fix Media Type
             };
 
-
-
             this.State = ComponentState.Content;
         }
 
         // TODO: need client side validation.
-        // TODO: better way to handle edit book component labels/placeholders
+
         public async void EditBookAsync(Book bookToEdit)
         {
             decimal uneditedListPrice = bookToEdit.ListPrice;
 
             try
             {
-                bookToEdit.Isbn = IsbnInput != null ? IsbnInput : bookToEdit.Isbn;
-                bookToEdit.Author = AuthorInput != null ? AuthorInput : bookToEdit.Author;
-                bookToEdit.Isbn13 = Isbn13Input != null ? Isbn13Input : bookToEdit.Isbn13;
-                bookToEdit.Title = TitleInput != null ? TitleInput : bookToEdit.Title;
-                bookToEdit.Subtitle = SubTitleInput != null ? SubTitleInput : bookToEdit.Subtitle;
-                bookToEdit.Publisher = PublisherInput != null ? PublisherInput : bookToEdit.Publisher;
+
+                bookToEdit.Isbn = !string.IsNullOrEmpty(Isbn) ? Isbn : bookToEdit.Isbn;
+                bookToEdit.Author = !string.IsNullOrEmpty(Author) ? Author : bookToEdit.Author;
+                bookToEdit.Isbn13 = !string.IsNullOrEmpty(Isbn13) ? Isbn13 : bookToEdit.Isbn13;
+                bookToEdit.Title = !string.IsNullOrEmpty(Title) ? Title : bookToEdit.Title;
+                bookToEdit.Subtitle = !string.IsNullOrEmpty(Subtitle) ? Subtitle : bookToEdit.Subtitle;
+                bookToEdit.Publisher = !string.IsNullOrEmpty(Publisher) ? Publisher : bookToEdit.Publisher;
+
                 bookToEdit.PublishDate = PublishDateInput != default ? PublishDateInput : bookToEdit.PublishDate;
 
                 if (Convert.ToDecimal(BookListPrice) == 0)
