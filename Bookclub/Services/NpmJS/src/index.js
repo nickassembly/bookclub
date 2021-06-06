f49cc5fee75edee6e5067f7efd70074713ac66ae
nickguerra <nickg@maliciousgames.com>
2021-06-06 07:52:34 -0500

    Added npm library to test interop

---
+++ Bookclub/Services/NpmJS/src/index.js
@@ -0,0 +1,14 @@
+﻿// test with infragistics js library radial gauge component
+import { ModuleManager } from 'igniteui-webcomponents-core';
+import { IgcRadialGaugeCoreModule } from 'igniteui-webcomponents-gauges';
+import { IgcRadialGaugeModule } from 'igniteui-webcomponents-gauges';
+
+ModuleManager.register(
+    IgcRadialGaugeCoreModule,
+    IgcRadialGaugeModule
+);
+
+window.updateValue = function (value) {
+    var rg = document.getElementById("rg");
+    rg.value = value;
+}
