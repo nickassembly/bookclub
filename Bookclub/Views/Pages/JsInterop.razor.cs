1cf614d3720081f67a36da2fa3e7a27cf807c02e
nickguerra <nickg@maliciousgames.com>
2021-06-07 18:56:28 -0500

    new branch, added comments

--- Bookclub/Views/Pages/JsInterop.razor.cs
+++ Bookclub/Views/Pages/JsInterop.razor.cs
@@ -11,6 +11,7 @@ namespace Bookclub.Views.Pages
 {
     partial class JsInterop
     {
+        // TODO: Integrate Isbn Library and Data fetching
         async void OnClick()
         {
             string isbn = "0034556678";
