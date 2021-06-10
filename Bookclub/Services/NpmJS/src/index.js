bacb511ce1dc1e09ef1bc8be445bf2a70c5cb472
nickguerra <nickg@maliciousgames.com>
2021-06-09 19:11:15 -0500

    added test code for isbn-node js library

--- Bookclub/Services/NpmJS/src/index.js
+++ Bookclub/Services/NpmJS/src/index.js
@@ -1,6 +1,16 @@
 ﻿
 
-window.getBookInfo = function (isbn) {
+window.getBookInfo = function (bookIsbn) {
 
-    console.log(isbn)
+    var isbn = require('node-isbn');
+
+    isbn.resolve(bookIsbn, function (err, book) {
+        if (err) {
+            console.log('Book not found', err);
+        } else {
+            console.log('Book found %j', book);
+        }
+    });
+
+    console.log("js method hit");
 }
